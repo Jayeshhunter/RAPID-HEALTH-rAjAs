8c4a2a26790f49a52cc1edaeca26a1920f424757
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2021-11-14 21:39:15 +0530

    feat: final commit

--- middlewares/patAuth.js
+++ middlewares/patAuth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/patient");
+require("dotenv").config();
 
 const checkPat = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
@@ -8,19 +9,23 @@ const checkPat = (req, res, next) => {
     const bearerToken = bearer[1];
     // req.token = bearerToken;
 
-    jwt.verify(bearerToken, "secretkey", async (err, decodedToken) => {
-      if (err) {
-        res.status(401).json({ error: "jwt invalid" });
-      } else {
-        let user = await User.findById(decodedToken.id);
-        if (user) {
-          req.user = user;
-          next();
+    jwt.verify(
+      bearerToken,
+      process.env.SECRET_KEY,
+      async (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ error: "jwt invalid" });
         } else {
-          res.status(401).json({ error: "user not found" });
+          let user = await User.findById(decodedToken.id);
+          if (user) {
+            req.user = user;
+            next();
+          } else {
+            res.status(401).json({ error: "user not found" });
+          }
         }
       }
-    });
+    );
   } else {
     res.status(401).json({ error: "token missing" });
   }
